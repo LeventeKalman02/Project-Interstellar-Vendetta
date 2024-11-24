7ee2f0071ea2943c292bde1047056a94e6665c93
EnhancedTarget <levente.kalman922@gmail.com>
2024-11-24 22:36:52 +0000

    disable hand models when grabbing objects script

---
+++ Assets/Scripts/DisableHandModelOnGrab.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class DisableHandModelOnGrab : MonoBehaviour
+{
+    public GameObject leftHandModel;
+    public GameObject rightHandModel;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(HideGrabbingHand);
+        grabInteractable.selectExited.AddListener(ShowGrabbingHand);
+    }
+
+    //hide hand model when grabbing an object
+    public void HideGrabbingHand(SelectEnterEventArgs args)
+    {
+        if(args.interactorObject.transform.tag == "LeftHand")
+        {
+            leftHandModel.SetActive(false);
+        }
+        else if(args.interactorObject.transform.tag == "RightHand")
+        {
+            rightHandModel.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("Hand tag not found");
+        }
+    }
+
+    //show hand model when releasing an object
+    public void ShowGrabbingHand(SelectExitEventArgs args)
+    {
+        if (args.interactorObject.transform.tag == "LeftHand")
+        {
+            leftHandModel.SetActive(true);
+        }
+        else if (args.interactorObject.transform.tag == "RightHand")
+        {
+            rightHandModel.SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("Hand tag not found");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Trash Disposal/TrashCan.cs
+++ Assets/Scripts/Trash Disposal/TrashCan.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class TrashCan : MonoBehaviour
 {
+    private void Start()
+    {
+        GetComponent<TriggerZone>().OnEnterEvent.AddListener(InsideTrash);
+    }
+
+
     // Called when a trash object enters the trash can
     public void InsideTrash(GameObject trash)
     {
