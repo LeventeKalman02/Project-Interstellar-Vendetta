0481e34399a2723c54b4d7454fac2b0d5b32e6f2
EnhancedTarget <levente.kalman922@gmail.com>
2025-03-31 14:38:53 +0100

    added variables and player references to enemyAI script

--- Assets/Scripts/EnemyScripts/EnemyAI.cs
+++ Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -1,9 +1,32 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
 
-    
+    public NavMeshAgent agent;
+
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //states
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+
+    private void Awake() {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,4 +37,5 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         
-  
+    }
+}
