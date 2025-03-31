52f0a49e6a59536c2ca8ec58cc2f1faadfe4925f
EnhancedTarget <levente.kalman922@gmail.com>
2025-03-31 14:43:35 +0100

    enemy patrolling function and search for walk point using raycast

--- Assets/Scripts/EnemyScripts/EnemyAI.cs
+++ Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -27,15 +27,48 @@ public class EnemyAI : MonoBehaviour
         player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+
+    private void Update() {
+        //check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) Attack();
     }
 
-    // Update is called once per frame
-    void Update()
+//enemy patrolling
+    private void Patrolling(){
+        //set walk point if null
+        if (!walkPointSet) SearchWalkPoint();
+        //walk to the set walk point
+        if (walkPointSet) agent.SetDestination(walkPoint);
+        //calculate distance to walk point
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f) {
+            walkPointSet = false; //walk point reached, set to false
+        }
+    }
+
+    private void SearchWalkPoint(){
+        //calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange); //return random value depending on walk point range
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) {
+            walkPointSet = true;
+        }
+    }
+    private void ChasePlayer()
     {
-        
+
+    }
+    private void Attack()
+    {
+
     }
 }
