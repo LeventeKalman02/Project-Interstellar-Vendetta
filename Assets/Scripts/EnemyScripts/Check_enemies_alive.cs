4cd2d75729f48373bddc23e5592f4fc27805e512
EnhancedTarget <levente.kalman922@gmail.com>
2025-04-22 14:18:52 +0100

    script to check if enemies are alive

---
+++ Assets/Scripts/EnemyScripts/Check_enemies_alive.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Check_enemies_alive : MonoBehaviour
+{
+
+    List<GameObject> listOfEnemies = new List<GameObject>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        listOfEnemies.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+        print(listOfEnemies.Count);
+    }
+
+    public void KilledOpponent(GameObject opponent)
+    {
+        if (listOfEnemies.Contains(opponent))
+        {
+            listOfEnemies.Remove(opponent);
+        }
+
+        print(listOfEnemies.Count);
+    }
+
+    public bool AreOpponentsDead()
+    {
+        if (listOfEnemies.Count <= 0)
+        {
+            // They are dead!
+            return true;
+        }
+        else
+        {
+            // They're still alive
+            return false;
+        }
+    }
+}
